0a177b4326d47781504bd74dacab6200818aacda
Maycon Santos <mayconzx1@gmail.com>
2025-06-06 17:46:03 -0300

    refactor: reestruturação de pastas separando back e front

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -28,7 +28,7 @@ const ChatPage = () => {
   }, [messages]);
 
   return (
-    <div className="min-h-screen bg-[url('/bg.png')] bg-cover bg-center flex flex-col justify-between p-4">
+    <div className="min-h-screen bg-[url('images/background.png')] bg-cover bg-center flex flex-col justify-between p-4">
       <h1 className="text-4xl text-orange-400 font-serif text-center mb-4">Talesmith</h1>
 
       <div className="flex flex-col overflow-y-auto gap-2 mb-4 max-h-[70vh] px-2">
