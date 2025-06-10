b2bc5186735839c1fdcb0ea0d10bc7670120fb02
nuneslg <lgnunees@gmail.com>
2025-06-10 19:14:12 -0300

    feat: inicializa o pacote app com a factory create_app e registro das rotas

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_cors import CORS
+from .routes import init_routes  # importa a função que registra as rotas
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Carrega as variáveis .env ao criar o app
 
 def create_app():
     app = Flask(__name__)
-
-    from .routes import main
-    app.register_blueprint(main)
-
+    CORS(app)
+    init_routes(app)  # registra as rotas usando o app
     return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
