acde356c515a35e44426a094600a461e57db84b5
Maycon Santos <mayconzx1@gmail.com>
2025-06-11 13:09:20 -0300

    feat: horário de envio adicionado em ChatBubble

--- frontend/src/pages/chat/_components/ChatBubble.jsx
+++ frontend/src/pages/chat/_components/ChatBubble.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+const ChatBubble = ({ text, author, time }) => {
 
-const ChatBubble = ({ text, author }) => {
   const isUser = author === "user";
   const alignment = isUser ? "justify-end" : "justify-start";
   const bubbleStyle = isUser ? "bg-orange-300 text-black rounded-br-none" : "bg-yellow-100 text-black rounded-bl-none";
 
   return (
     <div className={`flex flex-row ${alignment} items-center w-full`}>
-      <div className={`w-[45%] px-4 py-3 rounded-xl m-2 ${bubbleStyle} break-words`}>
+      <div className={`w-[60%] px-5 py-3 rounded-xl m-2 ${bubbleStyle} break-words`}>
         {text}
+        <p className='text-sm font-bold opacity-50 text-right'>{time}</p>
       </div>
     </div>
   );
