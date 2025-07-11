7752b1c78c90ac44f762b02c021a86b39a7a1960
nuneslg <lgnunees@gmail.com>
2025-07-10 22:10:13 -0300

    feat: adicionar banco ao app/__init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,15 +1,21 @@
 from flask import Flask
 from flask_cors import CORS
-from .routes import init_routes  # importa a função que registra as rotas
 from dotenv import load_dotenv
+from app.extensions import db  # Importa a instância do SQLAlchemy
+from .config import Config
+from .routes import register_blueprints  # Importa a função para registrar os blueprints
 import os
+
 
 load_dotenv()  # Carrega as variáveis .env ao criar o app
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)  # Carrega as configurações do config.py
     CORS(app)
-    init_routes(app)  # registra as rotas usando o app
+    db.init_app(app)  # Inicializa o SQLAlchemy com o app
+    register_blueprints(app)  # Registra os blueprints
+    
     return app
 
 if __name__ == "__main__":
