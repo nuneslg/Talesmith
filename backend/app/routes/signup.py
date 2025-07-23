ff39885e36ae10121a3b4dfcdea3293b6e49d9f6
Romero <romerofilho03@gmail.com>
2025-07-23 19:27:08 -0300

    feat:(added -password)

--- backend/app/routes/signup.py
+++ backend/app/routes/signup.py
@@ -9,10 +9,11 @@ def signup():
     dados = request.json
     username = dados.get('username')
     email = dados.get('email')
+    password = dados.get('password')
 
 
-    if not username or not email:
-        return jsonify({"erro": "Username e email são obrigatórios"}), 400
+    if not username or not email or not password:
+        return jsonify({"erro": "Username, email e senha são obrigatórios"}), 400
 
   
     usuario_existente = Usuario.query.filter(
@@ -22,12 +23,13 @@ def signup():
         return jsonify({"erro": "Usuário já existe com esse username ou email"}), 409
 
 
-    novo_usuario = Usuario(username=username, email=email)
+    novo_usuario = Usuario(username=username, email=email, password=password)
     db.session.add(novo_usuario)
     db.session.commit()
 
     return jsonify({
         "id": novo_usuario.id,
         "username": novo_usuario.username,
-        "email": novo_usuario.email
+        "email": novo_usuario.email,
+        "password": novo_usuario.password
     }), 201
