fe62bdcb8cfe7ace171a719556d70531470b2c21
Maycon Santos <mayconzx1@gmail.com>
2025-07-24 09:44:54 -0300

    feat: checagem de hash na senha

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request, jsonify
 from app.models.user import Usuario
 from app.extensions import db
-from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
+
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/api/auth')
 
@@ -9,16 +10,21 @@ auth_bp = Blueprint('auth', __name__, url_prefix='/api/auth')
 def login():
     dados = request.json
     email = dados.get('email')
-    username = dados.get('username')
     password = dados.get('password')
 
-    if not email or not username or not password:
+    
+    if not email or not password:
         return jsonify({"erro": "Email, senha e username são obrigatórios"}), 400
 
-    usuario = Usuario.query.filter_by(email=email, username=username).first()
+    usuario = Usuario.query.filter_by(email=email).first()
 
-    if not usuario or usuario.password != password:
-        return jsonify({"erro": "Usuário ou senha inválidos"}), 401
+    if not usuario:
+        return jsonify({"erro": "Usuario nao encontrado"}), 400
+    
+    is_valid_password = check_password_hash(usuario.password, password)
+    if not is_valid_password:
+        return  jsonify({"erro": "Credencias invalidas."}), 401
+ 
 
     return jsonify({
         "msg": "Login bem-sucedido",
