70fc5d403dcd0ccfdcc25e6e8ac0c44a1083adff
4lefee <mabs2@cin.ufpe.br>
2025-08-05 15:00:44 -0300

    ajuste banco de dados

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,6 @@
 from app import create_app
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 # Carrega as variáveis de ambiente do arquivo .env
@@ -7,4 +8,5 @@ load_dotenv()
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Pega a porta do Render ou 5000 local
+    app.run(host="0.0.0.0", port=port, debug=True)
